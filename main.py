beb498746de2193501a9d6b17e886b59f3efc499
Faith Omotayo <149096995+faithomotayo@users.noreply.github.com>
2024-12-01 18:10:12 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
-'''
+"""
 
-Author: Faith Omotayo. 
-Date: 28/01/24.
-Program: This is a password generator.
+Author: Faith Omotayo
+Date: 28/01/24
+Program: This is a password strength checker.
+Requirements :
+- Min of 1 special char, uppercase, lowercase and num.
+- Length >= 10.
+- No other chars allowed.
 
-'''
-import random
+"""
 
-print("Your password is : ")
+f, n, o , m = 0, 0, 0, 0
 
-chars = '!@#$%^&*()_?0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+password = input("Enter password here: ")
+specialchar = '#@!$^&%*()+_'
+uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+nums = '0123456789'
 
-password = ''
-for x in range(16): 
-    password += random.choice(chars)
+if (len(password) >= 10 ):
+    for i in password:
+        if (i in specialchar):
+            f+= 1
+        if (i in uppercase):
+            n += 1
+        if (i in lowercase):
+            o += 1 
+        if (i in nums):
+            m += 1
 
-print(password)
+
+
+if (f >= 1 and n >= 1 and o >= 1 and m >= 1 and f + n + o + m == len(password) ):
+    print("Password is valid ! ")
+else:
+    print("Password is invalid.Please try again\n")
+    password = False
+
+while password == False:
+    input("Enter password here: \n")
+    print("Password is invalid.Please try again\n\n")
+    
+
+
+
+
+
